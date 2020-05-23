80af4cbf10815e10ccabc98b3beb22119f8d35ac
Daniel Nunes de Almeida <daninunesdealmeida@gmail.com>
2020-05-23 14:31:15 -0300

    Projeto Security - V2

--- Projeto Security - V1/index.php
+++ Projeto Security - V1/index.php
@@ -1,54 +1,15 @@
 <?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
+    session_start();
 
-<head>
-	<meta charset="utf-8">
-	<title>CRUD - Cadastrar</title>
+ ?>
 
-
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="estilo.css">
-	<style>
-		body {
-			font-family: Georgia, 'Times New Roman', Times, serif;
-
-		}
-
-		#degrade1div {
-			background-image: linear-gradient(to top, #87CEFF, silver)
-		}
-
-		#semdecoration {
-			text-decoration: none;
-		}
-
-		#footer {
-			background-color: #e9ecef;
-			margin-left: 2%;
-			margin-right: 2%;
-			position: relative;
-		}
-
-
-		nav {
-			margin-left: 2%;
-			margin-right: 2%;
-			text-align: center;
-		}
-	</style>
-</head>
-
-
-<nav class="navbar navbar-dark bg-dark">
-	<span style="font-family: sans-serif;" class="navbar-text">
-		@ SECURITY
-	</span>
-</nav>
-
-<br>
+<?php require_once 'template/cabecalho.php'; ?>
+<section class="jumbotron text-center">
+  <div class="container">
+    <h1 class="jumbotron-heading"> Security </h1>
+		<p> <img src="imagens/logo.png" align = "center" style="width: 100px;">  </p>
+  </div>
+</section>
 
 <div id="degrade1div" class="container"><br>
 	<h2>Preencha com seus dados</h2>
@@ -56,15 +17,15 @@ session_start();
 	<form method="POST" action="processa.php">
 		<div class="form-group">
 			<label> Nome: </label>
-			<input type="text" class="form-control" name="nome" placeholder="Digite o nome" require><br><br>
+			<input type="text" class="form-control" name="nome" placeholder="Digite o nome" require><br>
 		</div>
 		<div class="form-group">
 			<label> E-mail: </label>
-			<input type="email" class="form-control" name="email" placeholder="Digite o e-mail" require><br><br>
+			<input type="email" class="form-control" name="email" placeholder="Digite o e-mail" require><br>
 		</div>
 		<div class="form-group">
 			<label> CPF: </label>
-			<input type="text" class="form-control" id="cpf" name="cpf" placeholder="Digite o CPF" onkeypress="$(this).mask('000.000.000-00');" require><br><br>
+			<input type="text" class="form-control" id="cpf" name="cpf" placeholder="Digite o CPF" onkeypress="$(this).mask('000.000.000-00');" require><br>
 		</div>
 		<input type="submit" class="btn btn-primary" value="Enviar">
 	</form>
@@ -77,15 +38,8 @@ session_start();
 	?>
 	<hr>
 	<div>
-		<h5>Após enviar os dados, para continuar <a style="color: darkred;" id="semdecoration"href="">CLIQUE AQUI.</a></h5>
+		<h5>Após enviar os dados, para continuar <a style="color: darkred;" id="semdecoration"href="https://www.upf.br/">CLIQUE AQUI.</a></h5>
 	</div>
-
 </body>
-<footer style="text-align: center;" id="footer" class="footer mt-auto py-3">
-	<div class="container">
-		<span class="text-muted">@ Security S.A - Construído por: Daniel de Almeida
-		</span>
-	</div>
-</footer>
-
+<?php require_once 'template/rodape.php'; ?>
 </html>
---
+++ Projeto Security - V1/template/cabecalho.php
@@ -0,0 +1,23 @@
+<?php define('BASE_URL', 'http://localhost/PHP'); ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Caution</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+<script src="jquery.maskedinput.js"></script>
+<script src="js.js"></script>
+</head>
+
+
+    <li><a href="<?= BASE_URL; ?>/index/index.php" class="text-white">Security</a></li>
